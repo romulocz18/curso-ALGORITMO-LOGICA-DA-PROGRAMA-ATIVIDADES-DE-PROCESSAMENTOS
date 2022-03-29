3e675b32e0e19521198147915438301a95af0e84
ROMULO CEZAR <101668013+romulocz18@users.noreply.github.com>
2022-03-28 23:38:03 -0300

    Algoritmo 2

---
+++ tipos_de_dados.pas
@@ -0,0 +1,19 @@
+program tipos_de_dados;
+var
+    salario:real;
+    idade:integer;
+    nome:string;
+begin
+write('Digite o nome: ');
+readln(nome);
+write('Digite sua idade: ');
+readln(idade);
+write('Digite seu salario: ');
+readln(salario);
+writeln('O nome = ',nome);
+writeln('O idade = ',idade);
+writeln('O salario =',salario);
+write('........FIM.........');
+
+readln();
+end.
