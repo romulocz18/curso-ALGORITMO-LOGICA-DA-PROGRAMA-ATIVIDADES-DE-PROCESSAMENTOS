c59606f5207633d25f579a65066285ac00ee89ef
ROMULO CEZAR <101668013+romulocz18@users.noreply.github.com>
2022-04-19 12:23:46 -0300

    modulo 5

---
+++ saldo_bancario.java
@@ -0,0 +1,15 @@
+package ex7;
+import javax.swing.JOptionPane;
+public class saldo_bancario 
+{
+          public static void main(String[]args)
+     {
+       float saldo, cheque_entrou, saldo_atual;
+       saldo = 500;
+       cheque_entrou = Float.parseFloat(JOptionPane.showInputDialog("Digite o valor de entrada do cheque.: "));
+       saldo_atual = saldo - cheque_entrou;
+       
+      JOptionPane.showMessageDialog(null,"Sua conta ficou com o saldo de.: "+saldo_atual);
+      ;
+     }
+}
